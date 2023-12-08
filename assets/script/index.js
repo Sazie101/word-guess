203e9554f5157574e8c46080496349012d3eac70
Samuel Yazie <samuelwassie169@gmail.com>
2023-12-08 11:04:15 -0600

    adding reset

--- assets/script/index.js
+++ assets/script/index.js
@@ -27,7 +27,7 @@ function shuffleArray(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
-const shuffledWords = shuffleArray(words);
+let shuffledWords = shuffleArray(words);
 const wordDisplay = select('.word-display');
 const userInput = select('.user-input');
 const scoreDisplay = select('.score');
@@ -41,7 +41,98 @@ const countdownSound = selectById('countdownSound');
 const correctAnswer = selectById('correctAnswer');
 const tickingClock = selectById('tickingClock');
 
-function startCounDown() {
+// function startCounDown() {
+//     countDown.innerText = '3';
+//     setTimeout(() => {
+//         countDown.innerText = '2';
+//         setTimeout(() => {
+//             countDown.innerText = '1';
+//             setTimeout(() => {
+//                 countDown.innerText = '';
+//                 everything.style.display = 'block';
+//                 userInput.focus();
+//                 startPage.style.display = 'none';
+//                 startGame();
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }
+
+// function startGame() {
+//     let score = 0;
+//     let timer = 15;
+
+//     function updateWord() {
+//         if (shuffledWords.length > 0) {
+//             const currentWord = shuffledWords.pop();
+//             wordDisplay.innerText = currentWord;
+//             userInput.value = ''; 
+//         } else {
+//             endGame();
+//         }
+//     }
+    
+//     function updateScore() {
+//         score++;
+//         scoreDisplay.innerText = `Score: ${score}`;
+//     }
+    
+//     function updateTimer() {
+//         timer--;
+//         timerDisplay.innerText = `Time remaining: ${timer}s`;
+
+//         if (timer <= 60) {
+//             timerDisplay.style.color = '#ffff00';
+//         } 
+        
+//         if (timer <= 10) {
+//             timerDisplay.style.color = '#ff0000';
+//             timerDisplay.classList.add('flash');
+//             tickingClock.play();
+//         }
+    
+//         if (timer === 0) {
+//             tickingClock.pause();
+//             timerDisplay.classList.remove('flash');
+//             endGame();
+//         }
+//     }
+
+//     updateWord();
+
+//     function endGame() {
+//         wordDisplay.innerText = 'Game Over!';
+//         userInput.disabled = true;
+//         clearInterval(gameInterval);
+//     }
+    
+//     onEvent('input', userInput, () => {
+//         if (userInput.value.trim() === wordDisplay.innerText) {
+//             correctAnswer.play();
+//             updateScore();
+//             updateWord();
+//         }
+//     });
+
+//     const gameInterval = setInterval(updateTimer, 1000);
+// }
+
+// onEvent('load', window, () => {
+//     everything.style.display = 'none';
+// });
+
+// onEvent('click', startBtn, () => {
+//     startBtn.style.display = 'none';
+//     countdownSound.play();
+//     startCounDown();
+// });
+
+let gameInterval; 
+let score;
+let timer;
+
+function startCountDown(score, timer) {
+    countdownSound.play();
     countDown.innerText = '3';
     setTimeout(() => {
         countDown.innerText = '2';
@@ -52,77 +143,93 @@ function startCounDown() {
                 everything.style.display = 'block';
                 userInput.focus();
                 startPage.style.display = 'none';
-                startGame();
+                countdownSound.pause();
+                function updateWord() {
+                    if (shuffledWords.length > 0) {
+                        const currentWord = shuffledWords.pop();
+                        wordDisplay.innerText = currentWord;
+                        userInput.value = ''; 
+                    } else {
+                        endGame();
+                    }
+                }
+
+                function updateScore() {
+                    score++;
+                    scoreDisplay.innerText = `Score: ${score}`;
+                }
+
+                function updateTimer() {
+                    timer--;
+                    timerDisplay.innerText = `Time remaining: ${timer}s`;
+
+                    if (timer <= 60) {
+                        timerDisplay.style.color = '#ffff00';
+                    } 
+
+                    if (timer <= 10) {
+                        timerDisplay.style.color = '#ff0000';
+                        timerDisplay.classList.add('flash');
+                        tickingClock.play();
+                    }
+
+                    if (timer === 0) {
+                        tickingClock.pause();
+                        timerDisplay.classList.remove('flash');
+                        endGame();
+                    }
+                }
+
+                updateWord();
+
+                function endGame() {
+                    wordDisplay.innerText = 'Game Over!';
+                    userInput.disabled = true;
+                    clearInterval(gameInterval);
+                }
+
+                onEvent('input', userInput, () => {
+                    if (userInput.value.trim() === wordDisplay.innerText) {
+                        correctAnswer.play();
+                        updateScore();
+                        updateWord();
+                    }
+                });
+
+                gameInterval = setInterval(updateTimer, 1000); 
             }, 1000);
         }, 1000);
     }, 1000);
 }
 
-function startGame() {
+function resetGame() {
+    // Reset shuffled words array
+    shuffledWords = shuffleArray(words.slice());
+    // Reset score and time
     let score = 0;
-    let timer = 99;
-
-    function updateWord() {
-        if (shuffledWords.length > 0) {
-            const currentWord = shuffledWords.pop();
-            wordDisplay.innerText = currentWord;
-            userInput.value = ''; 
-        } else {
-            endGame();
-        }
-    }
-    
-    function updateScore() {
-        score++;
-        scoreDisplay.innerText = `Score: ${score}`;
-    }
-    
-    function updateTimer() {
-        timer--;
-        timerDisplay.innerText = `Time remaining: ${timer}s`;
-
-        if (timer <= 60) {
-            timerDisplay.style.color = '#ffff00';
-        } 
-        
-        if (timer <= 20) {
-            timerDisplay.style.color = '#ff0000';
-            timerDisplay.classList.add('flash');
-            tickingClock.play();
-        }
-    
-        if (timer === 0) {
-            tickingClock.pause();
-            timerDisplay.classList.remove('flash');
-            endGame();
-        }
-    }
-
-    updateWord();
-
-    function endGame() {
-        wordDisplay.innerText = 'Game Over!';
-        userInput.disabled = true;
-        clearInterval(gameInterval);
-    }
-    
-    onEvent('input', userInput, () => {
-        if (userInput.value.trim() === wordDisplay.innerText) {
-            correctAnswer.play();
-            updateScore();
-            updateWord();
-        }
-    });
-
-    const gameInterval = setInterval(updateTimer, 1000);
+    let timer = 15;
+    scoreDisplay.innerText = 'Score: 0';
+    timerDisplay.innerText = 'Time remaining: 15s';
+    userInput.disabled = false;
+    clearInterval(gameInterval);
+    everything.style.display = 'none';
+    startPage.style.display = 'block';
+    onEvent('ended', countdownSound, startCountDown);
+    startCountDown(score, timer);
 }
+
 
 onEvent('load', window, () => {
     everything.style.display = 'none';
 });
 
 onEvent('click', startBtn, () => {
+    score = 0;
+    timer = 15;
     startBtn.style.display = 'none';
-    countdownSound.play();
-    startCounDown();
+    startCountDown(score, timer);
+});
+
+onEvent('click', resetBtn, () => {
+    resetGame();
 });
